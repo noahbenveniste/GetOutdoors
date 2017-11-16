ac7909ded4d86a8de0c6d2da5f5eca03ed1f5267
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-16 10:06:12 -0500

    Added method headers for methods from SortedList in SortedArrayList

--- Project3/src/edu/ncsu/csc216/get_outdoors/util/SortedArrayList.java
+++ Project3/src/edu/ncsu/csc216/get_outdoors/util/SortedArrayList.java
@@ -1,9 +1,51 @@
 package edu.ncsu.csc216.get_outdoors.util;
 
-public class SortedArrayList<E> {
+public class SortedArrayList<E> implements SortedList {
 
 	public SortedArrayList() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Comparable e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(Comparable e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Comparable get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Comparable e) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
