d98d66f788c0386a018fa4373138d2e46f7fdc18
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-17 16:14:50 -0500

    Created skeleton for ParkList
    -also added implements Comparable to Park's class header

--- Project3/src/edu/ncsu/csc216/get_outdoors/model/ParkList.java
+++ Project3/src/edu/ncsu/csc216/get_outdoors/model/ParkList.java
@@ -1,9 +1,98 @@
 package edu.ncsu.csc216.get_outdoors.model;
 
-public class ParkList {
+import java.util.Observable;
+import java.util.Observer;
 
+import edu.ncsu.csc216.get_outdoors.util.SortedLinkedList;
+
+/**
+ * 
+ * @author Noah Benveniste
+ */
+public class ParkList extends Observable implements Observer, Tabular {
+
+	/** */
+	private String name;
+	/** */
+	private int numParks;
+	/** */
+	private SortedLinkedList<Park> parks;
+	
+	/**
+	 * 
+	 */
 	public ParkList() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getName() {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @param description
+	 * @param snowChange
+	 * @return
+	 */
+	public boolean addPark(String name, String description, double snowChange) {
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public Park getParkAt(int index) {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int size() {
+		return this.parks.size();
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return this.parks.isEmpty();
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public int indexOfID(String id) {
+		return 0;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public Object[][] get2DArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
 	}
 
 }
