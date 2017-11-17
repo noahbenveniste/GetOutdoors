016ea0d5149c89a3ee0f7439f24f40d3751090da
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-17 12:39:53 -0500

    Added some javadoc headers to Trail and Difficulty

--- Project3/src/edu/ncsu/csc216/get_outdoors/model/Trail.java
+++ Project3/src/edu/ncsu/csc216/get_outdoors/model/Trail.java
@@ -3,7 +3,9 @@ package edu.ncsu.csc216.get_outdoors.model;
 import java.util.Observable;
 
 /**
+ * 
  * Trail is an OBSERVABLE class and is observed by TrailList.
+ * 
  * @author Noah Benveniste
  */
 public class Trail extends Observable {
