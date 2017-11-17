748570dcea27ea208e0bc589dbf8ea0ec565a4e4
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-16 21:46:59 -0500

    Started skeleton for Trail

--- Project3/src/edu/ncsu/csc216/get_outdoors/model/Trail.java
+++ Project3/src/edu/ncsu/csc216/get_outdoors/model/Trail.java
@@ -1,9 +1,27 @@
 package edu.ncsu.csc216.get_outdoors.model;
 
-public class Trail {
+import java.util.Observable;
+
+/**
+ * Trail is an OBSERVABLE class and is observed by TrailList.
+ * @author Noah Benveniste
+ */
+public class Trail extends Observable {
+	
+	/** */
+	private String trailID;
+	/** */
+	private String trailName;
+	/** */
+	private boolean closedForMaintenance;
+	/** */
+	private double snow;
+	/** */
+	private double distance;
+	
 
 	public Trail() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
