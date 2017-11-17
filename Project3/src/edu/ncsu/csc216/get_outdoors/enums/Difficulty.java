972f9d1a696e2ac1f374174808344d93d01d1c35
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-16 21:18:12 -0500

    Created Difficulty enum

--- Project3/src/edu/ncsu/csc216/get_outdoors/enums/Difficulty.java
+++ Project3/src/edu/ncsu/csc216/get_outdoors/enums/Difficulty.java
@@ -1,5 +1,16 @@
 package edu.ncsu.csc216.get_outdoors.enums;
 
+/**
+ * 
+ * @author Noah Benveniste
+ */
 public enum Difficulty {
-
+	
+	EASY,
+	MODERATE,
+	CHALLENGING,
+	DIFFICULT,
+	VERY_DIFFICULT,
+	EXTREME
+	
 }
