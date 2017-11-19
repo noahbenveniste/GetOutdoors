20e325d5a38930371519575156362e50c09a5db8
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-19 00:26:47 -0500

    Finished testing get2DArrayNoMaintenance()

--- Project3/test/edu/ncsu/csc216/get_outdoors/model/TrailListTest.java
+++ Project3/test/edu/ncsu/csc216/get_outdoors/model/TrailListTest.java
@@ -45,20 +45,20 @@ public class TrailListTest {
 	public void testAddTrail() {
 		//Try adding an invalid trail
 		assertFalse(t.addTrail(null, new SortedArrayList<Activity>(), false, 0.0, 1.0, Difficulty.EASY));
-		assertEquals(0, t.get2DArray().length);
+		assertEquals(0, t.size());
 		
 		//Add a valid trail, check that its ID is correct
 		assertTrue(t.addTrail("Valid Trail 1", new SortedArrayList<Activity>(), false, 0.0, 1.0, Difficulty.EASY));
-		assertEquals(1, t.get2DArray().length);
+		assertEquals(1, t.size());
 		assertEquals("park-0-0", t.get2DArray()[0][0]);
 		
 		//Try to add another invalid trail
 		assertFalse(t.addTrail(null, new SortedArrayList<Activity>(), false, 0.0, 1.0, Difficulty.EASY));
-		assertEquals(1, t.get2DArray().length);
+		assertEquals(1, t.size());
 		
 		//Add another valid trail, check that its ID is correct
 		assertTrue(t.addTrail("Valid Trail 2", new SortedArrayList<Activity>(), false, 0.0, 1.0, Difficulty.EASY));
-		assertEquals(2, t.get2DArray().length);
+		assertEquals(2, t.size());
 		assertEquals("park-0-1", t.get2DArray()[1][0]);
 	}
 	
@@ -99,7 +99,52 @@ public class TrailListTest {
 	 */
 	@Test
 	public void testGet2DArrayNoMaintenance() {
+		Activity a0 = new Activity("act-0", "bike", "mountain biking", false, 1);
+		Activity a1 = new Activity("act-1", "ski", "skiing", true, 4);
+		Activity a2 = new Activity("act-2", "run", "running", false, 2);
+		Activity a3 = new Activity("act-3", "ultimate frisbee", "frisbee", false, 5);
+		Activity a4 = new Activity("act-4", "snowshoe", "showshoeing", true, 8);
 		
+		SortedArrayList<Activity> a = new SortedArrayList<Activity>();
+		a.add(a0);
+		a.add(a1);
+		a.add(a2);
+		a.add(a3);
+		a.add(a4);
+		
+		t.addTrail("Trail 1", a, true, 0.0, 1.0, Difficulty.EASY);
+		t.addTrail("Trail 2", a, false, 0.0, 1.0, Difficulty.EASY);
+		t.addTrail("Trail 3", a, true, 0.0, 1.0, Difficulty.EASY);
+		t.addTrail("Trail 4", a, false, 0.0, 1.0, Difficulty.EASY);
+		t.addTrail("Trail 5", a, false, 0.0, 1.0, Difficulty.EASY);
+		t.addTrail("Trail 6", a, true, 0.0, 1.0, Difficulty.EASY);
+		
+		Object[][] o = t.get2DArrayNoMaintenance();
+		assertEquals(3, o.length);
+		
+		assertEquals("park-0-1", o[0][0]);
+		assertEquals("Trail 2", o[0][1]);
+		assertEquals(false, o[0][2]);
+		assertEquals(0.0, o[0][3]);
+		assertEquals(1.0, o[0][4]);
+		assertEquals(Difficulty.EASY, o[0][5]);
+		assertEquals(a, o[0][6]);
+		
+		assertEquals("park-0-3", o[1][0]);
+		assertEquals("Trail 4", o[1][1]);
+		assertEquals(false, o[1][2]);
+		assertEquals(0.0, o[1][3]);
+		assertEquals(1.0, o[1][4]);
+		assertEquals(Difficulty.EASY, o[1][5]);
+		assertEquals(a, o[1][6]);
+		
+		assertEquals("park-0-4", o[2][0]);
+		assertEquals("Trail 5", o[2][1]);
+		assertEquals(false, o[2][2]);
+		assertEquals(0.0, o[2][3]);
+		assertEquals(1.0, o[2][4]);
+		assertEquals(Difficulty.EASY, o[2][5]);
+		assertEquals(a, o[2][6]);
 	}
 	
 	/**
