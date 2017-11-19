c86d59aef691274fa10b03c36418abcef1e2eead
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-19 18:10:37 -0500

    Created skeleton for GetOutdoorsManagerTest

--- Project3/test/edu/ncsu/csc216/get_outdoors/GetOutdoorsManagerTest.java
+++ Project3/test/edu/ncsu/csc216/get_outdoors/GetOutdoorsManagerTest.java
@@ -5,15 +5,85 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Unit tests for GetOutdoorsManager
+ * 
+ * @author Noah Benveniste
+ */
 public class GetOutdoorsManagerTest {
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 	}
 
 	@Test
-	public void test() {
+	public void testGetOutdoorsManager() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void testIsChanged() {
+		fail();
+	}
+	
+	@Test
+	public void testSetChanged() {
+		fail();
+	}
+	
+	@Test
+	public void testGetFilename() {
+		
+	}
+	
+	@Test
+	public void testSetFilename() {
+		
+	}
+	
+	@Test
+	public void testGetNumTrailLists() {
+		
+	}
+	
+	@Test
+	public void testGetTrailListInt() {
+		
+	}
+	
+	@Test
+	public void testGetTrailLists() {
+		
+	}
+	
+	@Test
+	public void testGetActivities() {
+		
+	}
+	
+	@Test
+	public void testAddTrailListPark() {
+		
+	}
+	
+	@Test
+	public void testGetParks() {
+		
+	}
+	
+	@Test
+	public void testSaveDataFile() {
+		
+	}
+	
+	@Test
+	public void testOpenDataFile() {
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+	}
+	
 }
