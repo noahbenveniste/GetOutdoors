78f9f938846a2894870cb23419862b3fe48b1427
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-20 16:23:36 -0500

    Current version of GetOutdoorsManagerTest
    -pushing for pair programming

---
+++ Project3/test-files/expected2.md
@@ -0,0 +1,21 @@
+# Activities
+
+*	Bike	Biking	false	2
+*	Fish	Freshwater fishing	false	1
+*	Run	Running	false	5
+*	Ski	Skiing and Snowboarding	true	25
+
+# Parks
+
+*	Centennial Campus	Park contained on Engineering Campus	1.0
+*	Main Campus	Lovely park on Main Campus	0.0
+*	Pullen Park	Located on the northern end of campus	0.0
+
+# Trails
+
+*	Centennial Campus	Hunt Trail	false	0.0	0.75	EASY	Run	Running	false	5
+*	Centennial Campus	Lake Trail	true	0.0	2.5	EASY	Run	Running	false	5
+*	Centennial Campus	Oval Hill	false	0.0	1.0	CHALLENGING	Bike	Biking	false	2	Ski	Skiing and Snowboarding	true	25
+*	Main Campus	Bell Tower Trail	false	0.0	0.5	EASY	Bike	Biking	false	2	Run	Running	false	5
+*	Main Campus	Red Brick Road	true	0.0	1.5	MODERATE	Run	Running	false	5
+*	Pullen Park	Main trail	true	0.0	1.0	MODERATE	Bike	Biking	false	2
--- Project3/test/edu/ncsu/csc216/get_outdoors/GetOutdoorsManagerTest.java
+++ Project3/test/edu/ncsu/csc216/get_outdoors/GetOutdoorsManagerTest.java
@@ -5,6 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.ncsu.csc216.get_outdoors.enums.Difficulty;
+import edu.ncsu.csc216.get_outdoors.model.Activity;
+import edu.ncsu.csc216.get_outdoors.model.ActivityList;
+import edu.ncsu.csc216.get_outdoors.model.Park;
+import edu.ncsu.csc216.get_outdoors.model.TrailList;
+import edu.ncsu.csc216.get_outdoors.util.SortedArrayList;
+
 /**
  * Unit tests for GetOutdoorsManager
  * 
@@ -99,8 +106,51 @@ public class GetOutdoorsManagerTest {
 		manager.getParks().addPark("Pullen Park", "Located on the northern end of campus", 0.0);
 		assertEquals(3, manager.getParks().size());
 		assertEquals(3, manager.getNumTrailLists());
+		//Add a trail to the new park's trail list with its own list of activities, taken from manager.getActivities()
+		SortedArrayList<Activity> a = new SortedArrayList<Activity>();
+		a.add(manager.getActivities().getActivityAt(0));
+		manager.getTrailList(2).addTrail("Main trail", a, true, 0.0, 1.0, Difficulty.MODERATE);
+		//Save the current manager contents to a data file
+		manager.saveDataFile("test-files/expected.md");
+		
+		//Test that the exported file can be read back in
+		manager = null;
+		manager = new GetOutdoorsManager();
+		manager.openDataFile("test-files/expected.md");
+		assertEquals(4, manager.getActivities().size());
+		assertEquals(3, manager.getParks().size());
+		assertEquals(3, manager.getNumTrailLists());
+		Activity exp = new Activity("act-2", "Run", "Running", false, 5);
+		assertEquals(exp, manager.getTrailList(0).getTrailAt(0).getActivities().get(0));
+		manager.saveDataFile("test-files/expected2.md");
 		//fail("Not yet implemented");
 	}
+	
+	@Test
+    public void testAddGetTrails() {
+        setUp();
+        Park park3 = new Park("park-3", "Park 3", "Park 3 is for threes");
+        Park park4 = new Park("park-4", "Park 4", "Park 4 is for threes");
+        Park park5 = new Park("park-5", "Park 5", "Park 5 is for threes");
+        Park park6 = new Park("park-6", "Park 6", "Park 6 is for threes");
+        TrailList trailList3 = new TrailList(park3);
+        TrailList trailList4 = new TrailList(park4);
+        TrailList trailList5 = new TrailList(park5);
+        TrailList trailList6 = new TrailList(park6);
+        
+        assertEquals(-1 , manager.addTrailList(null));
+        assertEquals(0, manager.addTrailList(park3));
+        assertEquals(1, manager.addTrailList(park4));
+        assertEquals(2, manager.addTrailList(park5));
+        assertEquals(3, manager.addTrailList(park6));
+        assertEquals(-1 , manager.addTrailList(null));
+        assertEquals(4, manager.getNumTrailLists());
+        
+        assertEquals(trailList3, manager.getTrailList(0));
+        assertEquals(trailList4, manager.getTrailList(1));
+        assertEquals(trailList5, manager.getTrailList(2));
+        assertEquals(trailList6, manager.getTrailList(3));
+    }
 	
 	@Test
 	public void testUpdate() {
