1a21da686b3a4a4928d36fe06364ac64411ecf52
Noah Benveniste <nnbenven@ncsu.edu>
2017-11-22 18:12:48 -0500

    Added placeholder javadoc for TrailListTab and TrailTableModel

--- Project3/src/edu/ncsu/csc216/get_outdoors/ui/TrailTableModel.java
+++ Project3/src/edu/ncsu/csc216/get_outdoors/ui/TrailTableModel.java
@@ -8,32 +8,49 @@ package edu.ncsu.csc216.get_outdoors.ui;
  */
 public class TrailTableModel extends TableModel {
 
-    /** */
+    /** Default serial version ID */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructs a TrailTableModel
+	 * 
+	 * @param data an array containing Trail data from the TrailList
+	 */
 	public TrailTableModel(Object[][] data) {
 		super(data);
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Gets the number of columns
+	 */
 	@Override
 	public int getColumnCount() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * Gets the name for a column
+	 */
 	@Override
 	public String getColumnName(int col) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Gets the data for a row
+	 */
 	@Override
 	public Data getRowData(int row) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Sets the data for a row
+	 */
 	@Override
 	public void setData(Data d, int row) {
 		// TODO Auto-generated method stub
