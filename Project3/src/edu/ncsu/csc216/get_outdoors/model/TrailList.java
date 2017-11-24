32b2f0e4f02edbdb5b7908a90f8bcc4d6e3d01ed
demills <demills@ncsu.edu>
2017-11-24 11:47:22 -0500

    Update to TrailList.equals(), in attempt to fix hashCode bug.

--- Project3/src/edu/ncsu/csc216/get_outdoors/model/TrailList.java
+++ Project3/src/edu/ncsu/csc216/get_outdoors/model/TrailList.java
@@ -187,7 +187,7 @@ public class TrailList extends Observable implements Observer, Tabular {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((park == null) ? 0 : park.getName().hashCode());
+		result = prime * result + ((park == null) ? 0 : park.hashCode());
 
 		return result;
 	}
@@ -206,6 +206,9 @@ public class TrailList extends Observable implements Observer, Tabular {
 
 		TrailList otherTrailList = (TrailList) object;
 		String otherParkName = otherTrailList.getParkName();
+		if (otherTrailList.hashCode() == park.hashCode()) {
+			return true;
+		}
 
 		if (otherParkName.equals(park.getName())) {
 			return true;
